299307e9d65e2a49bf3799b494c4d3f8f5153dc7
MinJeong1221 <mjeong1998@naver.com>
2023-07-31 01:37:28 +0900

    eslint, prettier 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      포트폴리오 시작
-      
-    </div>
-  );
+  return <div className="App">포트폴리오 시작</div>;
 }
 
 export default App;
